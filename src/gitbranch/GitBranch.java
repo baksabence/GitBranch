349fb44bd543cd4ce6719f2c1c4ba09dfbbee8d3
baksa.bence <baksa.bence@FUJI113win10.szaleziakk.local>
2024-09-30 13:27:23 +0200

    olvashato kod

--- src/gitbranch/GitBranch.java
+++ src/gitbranch/GitBranch.java
@@ -6,7 +6,13 @@ public class GitBranch {
 
   
     public static void main(String[] args) {
-        
+        int i = GitBranch.fv();
     }
+
+    private static int fv() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
     
 }
